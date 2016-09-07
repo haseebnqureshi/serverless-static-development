223765578d9c62aeb66189dab1f86726c0afaef7
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2016-09-07 08:46:33 -0400

    cleaned up project, ensuring right assets build in www

---
+++ frontend/js/3-site.js
@@ -0,0 +1,8 @@
+jQuery(function($) {
+
+	console.debug({
+		jquery: $,
+		underscore: _
+	});
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,8 @@ var concat = require('gulp-concat');
 var minify = require('gulp-minify');
 var rename = require('gulp-rename');
 var shell = require('gulp-shell');
+var exec = require('child_process').execSync;
+var port = 3000;
 
 /*
 Paths
@@ -17,19 +19,29 @@ Paths
 
 var paths = {
 	index: __dirname + '/frontend',
-	js: __dirname + '/frontend/js/*.js',
-	css: __dirname + '/frontend/css/*.css',
-	scss: __dirname + '/frontend/scss/*.scss',
-	images: __dirname + '/frontend/images/*',
+	src: {
+		js: __dirname + '/frontend/js/*.js',
+		css: __dirname + '/frontend/css/*.css',
+		scss: __dirname + '/frontend/scss/*.scss',
+		images: __dirname + '/frontend/images/*',
+		fonts: __dirname + '/frontend/fonts/*'
+	},
+	js: __dirname + '/frontend/js',
+	css: __dirname + '/frontend/css',
+	scss: __dirname + '/frontend/scss',
+	images: __dirname + '/frontend/images',
+	fonts: __dirname + '/frontend/fonts',
 	dist: {
 		index: __dirname + '/www',
 		js: __dirname + '/www/js',
 		css: __dirname + '/www/css',
-		images: __dirname + '/www/images'
+		images: __dirname + '/www/images',
+		fonts: __dirname + '/www/fonts'
 	},
 	views: {
 		index: __dirname + '/frontend/views',
-		all: __dirname + '/frontend/views/*'
+		all: __dirname + '/frontend/views/*',
+		partials: __dirname + '/frontend/views/partials/*'
 	} 
 }
 
@@ -38,19 +50,19 @@ Gulp Tasks
 */
 
 gulp.task('lint', function() {
-	return gulp.src(paths.js)
+	return gulp.src(paths.src.js)
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'));
 });
 
 gulp.task('sass', function() {
-	return gulp.src(paths.scss)
+	return gulp.src(paths.src.scss)
 		.pipe(sass())
 		.pipe(gulp.dest(paths.css));
 });
 
 gulp.task('scripts', function() {
-	return gulp.src(paths.js)
+	return gulp.src(paths.src.js)
 		.pipe(concat('bundle.js'))
 		.pipe(gulp.dest(paths.dist.js))
 		.pipe(minify({
@@ -62,17 +74,19 @@ gulp.task('scripts', function() {
 });
 
 gulp.task('stylesheets', function() {
-	return gulp.src(paths.css)
+	return gulp.src(paths.src.css)
 		.pipe(concat('bundle.css'))
-		.pipe(gulp.dest(paths.dist.css))
-		.pipe(rename('bundle.min.css'))
-		.pipe(minify())
 		.pipe(gulp.dest(paths.dist.css));
 });
 
 gulp.task('images', function() {
-	return gulp.src(paths.images)
+	return gulp.src(paths.src.images)
 		.pipe(gulp.dest(paths.dist.images));
+});
+
+gulp.task('fonts', function() {
+	return gulp.src(paths.src.fonts)
+		.pipe(gulp.dest(paths.dist.fonts));
 });
 
 gulp.task('views', shell.task([
@@ -80,14 +94,22 @@ gulp.task('views', shell.task([
 ]));
 
 gulp.task('watch', function() {
-	gulp.watch(paths.images, ['images']);
-	gulp.watch(paths.js, ['scripts']);
-	gulp.watch(paths.scss, ['sass']);
-	gulp.watch(paths.css, ['stylesheets']);
+	gulp.watch(paths.src.fonts, ['fonts']);
+	gulp.watch(paths.src.images, ['images']);
+	gulp.watch(paths.src.js, ['scripts']);
+	gulp.watch(paths.src.scss, ['sass']);
+	gulp.watch(paths.src.css, ['stylesheets']);
 	gulp.watch(paths.views.all, ['views']);
+	gulp.watch(paths.views.partials, ['views']);
 });
 
-gulp.task('default', ['sass', 'scripts', 'stylesheets', 'views', 'watch']);
+gulp.task('render', ['sass', 'scripts', 'stylesheets', 'views', 'images', 'fonts']);
+
+gulp.task('default', ['render', 'watch']);
+
+gulp.task('start', ['default'], function() {
+	exec(`open http://localhost:${port}/`);
+});
 
 /*
 Express Serving Static Files
@@ -96,4 +118,4 @@ Express Serving Static Files
 var express = require('express');
 var app = express();
 app.use(express.static(paths.dist.index));
-app.listen(3000);
+app.listen(port);
